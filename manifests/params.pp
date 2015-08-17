feda19321bc962bf8c87d85833819ebb616945c3
Mike Lehner <mlehner616+github@gmail.com>
2015-08-17 13:54:42 -0700

    Use widely-known osfamily fact to set cfg location
    Ran into an issue on Amazon linux because this module doesn't use the standard osfamily fact to identify RedHat family OSes. It instead explicitly lists three of many that should be compatible based on the configuration path.

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,9 +4,9 @@
 class varnish::params {
 
   # set Varnish conf location based on OS
-  $conf_file_path = $::operatingsystem ? {
-    /(?i:Centos|RedHat|OracleLinux)/  => '/etc/sysconfig/varnish',
-    default                           => '/etc/default/varnish',
+  $conf_file_path = $::osfamily ? {
+    'RedHat' => '/etc/sysconfig/varnish',
+    default  => '/etc/default/varnish',
   }
 
   $version = $varnish::version ? {
