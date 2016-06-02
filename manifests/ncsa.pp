acf9b6296044c3b1eb4c195524faf8cad9f95bfb
Joshua Spence <josh@freelancer.com>
2016-06-02 20:16:15 +1000

    Improve `/etc/init.d/varnishncsa`
    Improve `/etc/init.d/varnishncsa` to improve support for custom log formats.

--- manifests/ncsa.pp
+++ manifests/ncsa.pp
@@ -1,14 +1,27 @@
 class varnish::ncsa (
   $enable = true,
   $varnishncsa_daemon_opts = undef,
+  $log_format = undef,
 ) {
 
   file { '/etc/default/varnishncsa':
-    ensure  => 'present',
+    ensure  => 'file',
     mode    => '0644',
     owner   => 'root',
     group   => 'root',
     content => template('varnish/varnishncsa-default.erb'),
+    notify  => Service['varnishncsa'],
+  }
+
+  # TODO: This is a hacky workaround for issues with `start-stop-daemon`
+  # misinterpretting quoted arguments. See http://stackoverflow.com/q/1661193
+  # and also https://github.com/varnishcache/pkg-varnish-cache/pull/33.
+  file { '/etc/init.d/varnishncsa':
+    ensure  => 'file',
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+    content => template('varnish_cache/varnishncsa.debian.erb'),
     notify  => Service['varnishncsa'],
   }
 
