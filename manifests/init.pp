123b3a86ca1976f869b0ab00d853363ecf0ca4ac
Joshua Spence <josh@freelancer.com>
2016-07-26 12:06:44 +1000

    Add the ability to specify the identity of the Varnish server

--- manifests/init.pp
+++ manifests/init.pp
@@ -70,6 +70,7 @@ class varnish (
   $add_repo                     = true,
   $manage_firewall              = false,
   $varnish_conf_template        = 'varnish/varnish-conf.erb',
+  $varnish_identity             = undef,
   $additional_parameters        = {},
 ) {
 
