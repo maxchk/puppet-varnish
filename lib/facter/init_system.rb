3d5b3d7435a8d5aced3d9a0b3b6b6ae97ce15467
Emerson Prado <emerson.prado@dataprev.gov.br>
2017-09-28 17:29:58 -0300

    Change if != to unless in init_system custom fact for clarity

--- lib/facter/init_system.rb
+++ lib/facter/init_system.rb
@@ -2,7 +2,7 @@ Facter.add(:init_system) do
   confine :kernel => 'Linux'
   setcode do
     proc_1 = Facter::Core::Execution::exec('ps --no-headers -p 1').split(' ')[-1]
-    if proc_1 != 'init'
+    unless proc_1 == 'init'
       proc_1
     else
       path_exec = Facter::Core::Execution::exec("ls -l $(which #{proc_1})").split(' ')[-1]
