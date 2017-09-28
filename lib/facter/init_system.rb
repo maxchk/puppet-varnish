fce9705a7a06129039eebb27f82d442a28de129a
Emerson Prado <emerson.prado@dataprev.gov.br>
2017-09-28 16:25:20 -0300

    Check providing package for PID 1 executable in init_system fact

--- lib/facter/init_system.rb
+++ lib/facter/init_system.rb
@@ -6,7 +6,18 @@ Facter.add(:init_system) do
       proc_1
     else
       path_exec = Facter::Core::Execution::exec("ls -l $(which #{proc_1})").split(' ')[-1]
-      path_exec.split('/')[-1]
+      file_exec = path_exec.split('/')[-1]
+      unless file_exec == 'init'
+        file_exec
+      else
+        case Facter.value(:osfamily)
+        when 'Debian', 'Ubuntu'
+          pkg_exec = Facter::Core::Execution::exec("dpkg-query -S #{path_exec}").split(' ')[0]
+        when 'RedHat'
+          pkg_exec = Facter::Core::Execution::exec("rpm -qf #{path_exec}").split('-')[0]
+        end
+        pkg_exec.downcase[/systemd|upstart|init/]
+      end
     end
   end
 end
