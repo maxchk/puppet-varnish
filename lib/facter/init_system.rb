481cbd40261a583c78320d5d8d0c442a3dce4b7b
Emerson Prado <emerson.prado@dataprev.gov.br>
2017-09-28 16:24:26 -0300

    Deference symlink in executable path for PID 1 in init_system fact

--- lib/facter/init_system.rb
+++ lib/facter/init_system.rb
@@ -1,6 +1,12 @@
 Facter.add(:init_system) do
   confine :kernel => 'Linux'
   setcode do
-    Facter::Core::Execution::exec('ps --no-headers -p 1').split(' ')[-1]
+    proc_1 = Facter::Core::Execution::exec('ps --no-headers -p 1').split(' ')[-1]
+    if proc_1 != 'init'
+      proc_1
+    else
+      path_exec = Facter::Core::Execution::exec("ls -l $(which #{proc_1})").split(' ')[-1]
+      path_exec.split('/')[-1]
+    end
   end
 end
